3e30317a25afe43ceb831e0ddf7aaaab7773cb75
Sybren Boland <sybren.boland@ns.nl>
2021-07-29 18:43:02 +0200

    feat: Replace package name in introspection bean class

--- src/main/java/nl/sybrenbolandit/genie/grpc/sub/InsertIntrospectionBean.java
+++ src/main/java/nl/sybrenbolandit/genie/grpc/sub/InsertIntrospectionBean.java
@@ -2,13 +2,16 @@ package nl.sybrenbolandit.genie.grpc.sub;
 
 import picocli.CommandLine;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 import java.util.concurrent.Callable;
+import java.util.stream.IntStream;
 
 @CommandLine.Command(
     name = "insert-introspection-bean",
@@ -23,6 +26,11 @@ public class InsertIntrospectionBean implements Callable<Integer> {
       defaultValue = ".")
   private Path location;
 
+  @CommandLine.Option(
+          names = {"-p", "--package"},
+          description = "The package of the new class")
+  private String packageName;
+
   public static void main(String... args) {
     int exitCode = new CommandLine(new InsertIntrospectionBean()).execute(args);
     System.exit(exitCode);
@@ -32,16 +40,37 @@ public class InsertIntrospectionBean implements Callable<Integer> {
   public Integer call() throws IOException {
     System.out.printf("Insert introspection bean into %s%n", location);
 
-    InputStream sourceFile = fetchSourceFile();
-    copyFile(sourceFile);
+    String sourceFile = fetchSourceFile();
+    InputStream completeSource = setPackage(sourceFile);
+    copyFile(completeSource);
 
     return 0;
   }
 
-  private InputStream fetchSourceFile() throws IOException {
-    return Optional.ofNullable(
+  private String fetchSourceFile() throws IOException {
+    InputStream inputStream = Optional.ofNullable(
             InsertIntrospectionBean.class.getResourceAsStream("/grpc/" + FILE_NAME))
         .orElseThrow(() -> new IllegalStateException("Source file not found!"));
+
+    return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+  }
+
+  private InputStream setPackage(String source) {
+    String evaluatedPackageName =
+        Optional.ofNullable(packageName)
+            .orElse(evaluatePackageFromLocation());
+
+    String newSource = source.replace("@packageName@", evaluatedPackageName);
+
+    return new ByteArrayInputStream(newSource.getBytes(StandardCharsets.UTF_8));
+  }
+
+  private String evaluatePackageFromLocation() {
+    return IntStream.range(4, location.getNameCount())
+            .mapToObj(i -> location.getName(i))
+            .map(Path::toString)
+            .reduce((p1, p2) -> p1 + "." + p2)
+            .orElse("");
   }
 
   private void copyFile(InputStream source) throws IOException {
